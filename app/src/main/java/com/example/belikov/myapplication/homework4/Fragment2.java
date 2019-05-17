91cec79f07cc145a672e29136ed9c06ec9bb5539
Belikov <Sergeybelik86@gmail.com>
2019-05-17 14:56:34 +0400

    ДЗ № 7

--- app/src/main/java/com/example/belikov/myapplication/homework4/Fragment2.java
+++ app/src/main/java/com/example/belikov/myapplication/homework4/Fragment2.java
@@ -22,6 +22,6 @@ public class Fragment2 extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment2, container, true);
+        return inflater.inflate(R.layout.fragment2, container, false);
     }
 }
